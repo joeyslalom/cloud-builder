6d48960a0b1466a36836c21e8bce522de0ab11d8
joeyt <joeyt@slalom.com>
2020-09-16 16:37:00 -0700

    Test not running gcloud auth

--- script.sh
+++ script.sh
@@ -3,10 +3,11 @@
 cd dest
 
 echo "gcloud auth"
-gcloud auth configure-docker
+#gcloud auth configure-docker gcr.io
 
 echo "docker build"
-docker build -t gcr.io/gcp-pso-bfg-playground/target .
+docker build -q -t gcr.io/gcp-pso-bfg-playground/target .
 
+# access scope for Storage must be Read Write in order to push to GCR
 echo "docker push"
 docker push gcr.io/gcp-pso-bfg-playground/target
